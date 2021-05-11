25cf10bfbc7e89e699f8357e40824a443b6d1d4f
Dzheremilz <trempejeremie@gmail.com>
2021-05-11 18:52:35 +0200

    order

--- exercices-solidity-etherum/SimpleStorage.sol
+++ exercices-solidity-etherum/SimpleStorage.sol
@@ -3,14 +3,13 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    
     uint256 private _storedData;
-    
-    function storedData() public view returns (uint256) {
-        return _storedData;
-    }
-    
+
     function setStoredData(uint256 value) public {
         _storedData = value;
+    }
+
+    function storedData() public view returns (uint256) {
+        return _storedData;
     }
 }
