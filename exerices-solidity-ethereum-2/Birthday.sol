c38ba262ec81838b28f00388781edfd969eb9a11
Dzheremilz <trempejeremie@gmail.com>
2021-05-13 19:37:31 +0200

    birthday

--- exerices-solidity-ethereum-2/Birthday.sol
+++ exerices-solidity-ethereum-2/Birthday.sol
@@ -8,22 +8,24 @@ contract Birthday {
     using Address for address payable;
 
     address private _receiver;
-    uint256 private _today;
-    uint256 private _dayBeforeBirthday;
+    uint256 private _dateBirthday;
 
-    event Received(address sender, uint256 amount);
+    event Received(address indexed sender, uint256 amount);
 
-    constructor(address receiver_, uint256 dayBeforeBirthday_) {
+    constructor(address receiver_, uint256 dayBeforeBirthday) {
         _receiver = receiver_;
-        _today = block.timestamp;
-        _dayBeforeBirthday = dayBeforeBirthday_;
+        _dateBirthday = block.timestamp + (dayBeforeBirthday * 1 minutes); // change by 'days', use minutes for test purpose
     }
 
     receive() external payable {
+        require(block.timestamp < _dateBirthday, "Birthday: it's too late");
+        require(msg.value > 0, "Birthday: you cannot send 0 ether");
         emit Received(msg.sender, msg.value);
     }
 
     function offer() public payable {
+        require(block.timestamp < _dateBirthday, "Birthday: it's too late");
+        require(msg.value > 0, "Birthday: you cannot send 0 ether");
         emit Received(msg.sender, msg.value);
     }
 
@@ -33,9 +35,10 @@ contract Birthday {
             "Birthday: you are not the person for whom this contract is intended"
         );
         require(
-            block.timestamp >= _today + _dayBeforeBirthday * 1 days,
+            block.timestamp >= _dateBirthday,
             "Birthday: this is not the right time yet"
         );
-        payable(msg.sender).sendValue(address(this).balance);
+        //payable(msg.sender).sendValue(address(this).balance);
+        selfdestruct(payable(msg.sender)); // no, bad idea, when you selfdestruct states variables lose their data
     }
 }
