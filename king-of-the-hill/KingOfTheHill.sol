05b226fdb6a4cbca30bd7dd1c9ba1c31142fe94c
Dzheremilz <trempejeremie@gmail.com>
2021-05-14 17:40:22 +0200

    Natspec

--- king-of-the-hill/KingOfTheHill.sol
+++ king-of-the-hill/KingOfTheHill.sol
@@ -34,6 +34,13 @@ contract KingOfTheHill is Ownable {
         emit KingChanged(msg.sender);
     }
 
+    /**
+     * @notice some modifier:
+     * - atLeastDouble : Check the value send to become the next King, it must be twice the actual pot (contract balance)
+     * - notPotOwner : Check the actual King is not bidding on himself
+     * - notOwner : The owner of the contract cannot play his own game, he already gain 10% of the pot on victory
+     */
+
     modifier atLeastDouble() {
         require(
             msg.value >= (address(this).balance - msg.value) * 2,
@@ -58,6 +65,12 @@ contract KingOfTheHill is Ownable {
         _;
     }
 
+    /**
+     * @notice receive function and potOffering are use to take in the ether from player
+     * @dev the address and value receive are transmitted to the private function _potOffering,
+     * the principal function of our contract
+     */
+
     receive() external payable notOwner notPotOwner atLeastDouble {
         _potOffering(msg.sender, msg.value);
     }
@@ -65,6 +78,15 @@ contract KingOfTheHill is Ownable {
     function potOffering() external payable notOwner notPotOwner atLeastDouble {
         _potOffering(msg.sender, msg.value);
     }
+
+    /**
+     * @notice some view function use to display (return) the state of our contract:
+     * - blockNumber : the actual block number
+     * - blockNumberToWin : the block to reach for the king to win
+     * - pot: the actual balance of the contract
+     * - pot2: twice the actual balance, the value to send to become the next King
+     * - potOwner: address of the actual pot owner (king)
+     */
 
     function blockNumber() external view returns (uint256) {
         return block.number;
@@ -85,6 +107,18 @@ contract KingOfTheHill is Ownable {
     function potOwner() external view returns (address) {
         return _potOwner;
     }
+
+    /**
+     * @notice our core function: _potOffering, the function manages 2 distinct cases:
+     * - if _blockNumberToWin <= block.number == true
+     * it mean the previous king has won, the function then need to distribute the price between the king (80% of the pot),
+     * the owner of the contract (10%) and the seed (the remaining 10%), use has the next turn of this game. The new King is refund the difference
+     * between the ether sent and the amount use for the new pot.
+     * - else
+     * the bidder become the new king and if necessary is refund the difference between his bid and twice the pot (bid - pot * 2 = refund value)
+     *
+     * finally the new king is crowned, the new block number to reach is setup and a new turn begin
+     */
 
     function _potOffering(address account, uint256 amount) private {
         uint256 currentPot = address(this).balance - amount;
